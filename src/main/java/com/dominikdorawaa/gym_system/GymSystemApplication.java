b9d5c0a451fd4cde41291a5c2d6c1463fcccc869
domin <dominikdorawa12@gmail.com>
2026-05-11 22:18:26 +0200

    chore: initial project

---
+++ src/main/java/com/dominikdorawaa/gym_system/GymSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dominikdorawaa.gym_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymSystemApplication.class, args);
+	}
+
+}
